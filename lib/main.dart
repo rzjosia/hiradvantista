74c47059c27d478b803319cceac6c3f650242534
RAZAFINJATOVO Josia <rzjosia@gmail.com>
2023-01-08 21:47:54 +0100

    add about page

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,66 @@
 import 'package:flutter/material.dart';
 import 'package:hiradvantista/screens/Home.dart';
+import 'package:hiradvantista/screens/about.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _selectedIndex = 0;
+
+  static const navigationItems = [
+    {
+      'title': 'Fihirana advantista',
+      'label': 'Accueil',
+      'icon': Icons.home,
+      'screen': Home(),
+    },
+    {
+      'title': 'A propos',
+      'label': 'A propos',
+      'icon': Icons.info,
+      'screen': About(),
+    },
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Fihirana advantista',
+      title: "Fihirana advantista",
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hira advantista'),
+          title: Text("${navigationItems[_selectedIndex]['title']}"),
         ),
-        body: const Home(),
+        body: navigationItems[_selectedIndex]['screen'] as Widget,
+        bottomNavigationBar: BottomNavigationBar(
+          items: <BottomNavigationBarItem>[
+            ...navigationItems.map((item) => BottomNavigationBarItem(
+                  icon: Icon(item['icon'] as IconData),
+                  label: item['label'] as String,
+                  tooltip: item['title'] as String,
+                ))
+          ],
+          currentIndex: _selectedIndex,
+          selectedItemColor: Colors.green,
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,24 +7,23 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hiradvantista/constants/about_constant.dart';
 
 import 'package:hiradvantista/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('About page show developer email', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text("Home"), findsOneWidget);
+    expect(find.text("A propos"), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.byIcon(Icons.info));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.textContaining(AboutApp.description), findsOneWidget);
+    expect(find.textContaining(AboutApp.author), findsOneWidget);
+    expect(find.textContaining(AboutApp.developerEmail), findsOneWidget);
   });
 }
